36c77a9034315625ec688d339b84f6bef0cf3de2
murilo-simoes <murilorsimoes@gmail.com>
2022-02-17 18:13:22 -0300

    The first index ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import jumboData from './fixtures/jumbo'
+import Jumbotron from './components/jumbotron';
 
 function App() {
   return (
-    <p>Hello World!</p>
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+
+          <Jumbotron.Pane>
+              <Jumbotron.Image src={item.image} alt={item.alt}></Jumbotron.Image>
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+      </Jumbotron.Container>
   );
 }
+
 
 export default App;
